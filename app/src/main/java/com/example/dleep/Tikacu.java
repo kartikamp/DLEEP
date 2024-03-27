40be815b0493e598e62e14322bf74e9e93390720
kartikampr <2110512127@mahasiswa.upnvj.ac.id>
2024-03-27 22:49:04 +0700

    Nambahin EditProfil

---
+++ app/src/main/java/com/example/dleep/Tikacu.java
@@ -0,0 +1,14 @@
+package com.example.dleep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tikacu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tikacu);
+    }
+}
